520e55b577e3163eda409098a324616150672f89
melferium <ferdinand.melchor@hotmail.com>
2018-11-11 22:17:09 +0800

    edit config/application.rb and model/user.rb as instructed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+  
+  # added two line below as instructed in Setup Milia and Devise - Text directions and code section
+  acts_as_universal_and_determines_account
+  has_one :member, :dependent => :destroy
+
 end
